77729d4205d02e52b6bca63528bab34f11455d56
Rempacious <rempacious@gmail.com>
2023-06-30 13:46:49 +0800

    Update sh

--- patch_perseus.sh
+++ patch_perseus.sh
@@ -26,12 +26,12 @@ chmod +x apkeep
 
 # Download Azur Lane
 download_azurlane () {
-    if [ ! -f "com.hkmanjuu.azurlane.gp" ]; then
+    if [ ! -f "com.hkmanjuu.azurlane.gp.xapk" ]; then
     ./apkeep -a com.hkmanjuu.azurlane.gp .
     fi
 }
 
-if [ ! -f "com.hkmanjuu.azurlane.gp" ]; then
+if [ ! -f "com.hkmanjuu.azurlane.gp.apk" ]; then
     echo "Get Azur Lane apk"
     download_azurlane
     unzip -o com.hkmanjuu.azurlane.gp.xapk -d AzurLane
